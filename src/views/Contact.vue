ce63b7928aec168599275e12d047ac6d57b45fbc
pareena <pareenasumbli@gmail.com>
2020-08-16 18:02:48 -0400

    form pt2
    deleted unnecessary code

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -9,9 +9,6 @@ width="640" height="3039" frameborder="0" marginheight="0" marginwidth="0">How Y
 
 <script>
 
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdpdpIjhpTaV0m5GBcYAOgKcrpi7VWseBG8LbzggkuykZ0gIw/viewform?embedded=true" 
-width="640" height="3039" frameborder="0" marginheight="0" marginwidth="0">How You Can Join Us</iframe>
-
 import ContactForm from "@/components/Contact_Form.vue"
 
 export default {
