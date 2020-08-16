8a50222c45d634df46c9cbe1be8931c3020f4093
pareena <pareenasumbli@gmail.com>
2020-08-16 18:01:48 -0400

    fixed form
    google form pt 2

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,8 @@
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdpdpIjhpTaV0m5GBcYAOgKcrpi7VWseBG8LbzggkuykZ0gIw/viewform?embedded=true" 
-width="640" height="3039" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
 <template>
     <div>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdpdpIjhpTaV0m5GBcYAOgKcrpi7VWseBG8LbzggkuykZ0gIw/viewform?embedded=true" 
+width="640" height="3039" frameborder="0" marginheight="0" marginwidth="0">How You Can Join Us</iframe>
         <ContactForm />
     </div>
 </template>
