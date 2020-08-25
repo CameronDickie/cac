635ab7897df6617292c647346af2c538c0710eae
CameronDickie <cameronbdickie@gmail.com>
2020-08-25 10:57:36 -0400

    Made Centering Changes
    Modified the contact page to contain headers plus centered the google form and removed the doubling of the tag

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,23 +1,23 @@
 <template>
-  <div>
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSdpdpIjhpTaV0m5GBcYAOgKcrpi7VWseBG8LbzggkuykZ0gIw/viewform?embedded=true"
-      width="640"
-      height="3039"
-      frameborder="0"
-      marginheight="0"
-      marginwidth="0"
-    >How You Can Join Us</iframe>
+  <v-container>
+    <v-card flex class="text-center">
+      <v-card-title>How You Can Join Us</v-card-title>
+    </v-card>
+    <v-flex class="text-center justify-center mx-auto">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdpdpIjhpTaV0m5GBcYAOgKcrpi7VWseBG8LbzggkuykZ0gIw/viewform?embedded=true"
+        width="640px"
+        height="3450px"
+        frameborder="0"
+      ></iframe>
+    </v-flex>
+
+    <v-card flex class="text-left">
+      <v-card-title>Send us an e-mail!</v-card-title>
+      <v-card-subtitle>This is the best way to get into contact with us</v-card-subtitle>
+    </v-card>
     <ContactForm />
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSdpdpIjhpTaV0m5GBcYAOgKcrpi7VWseBG8LbzggkuykZ0gIw/viewform?embedded=true"
-      width="640"
-      height="3039"
-      frameborder="0"
-      marginheight="0"
-      marginwidth="0"
-    >Loading…</iframe>
-  </div>
+  </v-container>
 </template>
 
 <script>
